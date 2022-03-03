54be28b6fc630d55142c89a38af17daf19962d28
HP <thuytien@ubuntu.ubuntu-domain>
2022-03-03 09:01:48 +0700

    update

--- Book/database/factories/PermissionFactory.php
+++ Book/database/factories/PermissionFactory.php
@@ -4,6 +4,7 @@ namespace Database\Factories;
 
 use App\Models\Permission;
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Str;
 
 class PermissionFactory extends Factory
@@ -15,30 +16,41 @@ class PermissionFactory extends Factory
      */
     public function definition()
     {
-        $permissions = [
-            'role-list',
-            'role-create',
-            'role-edit',
-            'role-delete'
-         ];
+        // $permissions = [
+        //     'role-list',
+        //     'role-create',
+        //     'role-edit',
+        //     'role-delete'
+        //  ];
 
-         foreach ($permissions as $permission) {
-             return
-              Permission::create([
-                  'name' => $permission,
-                    'slug' => $permission
-                ]);
-         }
+        //  foreach ($permissions as $permission) {
+        //      return
+        //       Permission::create([
+        //           'name' => $permission,
+        //             'slug' => $permission
+        //         ]);
+        //  }
 
-         DB::table('permissions')->insert([
+        //  DB::table('permissions')->insert([
+        //     ['name' => 'Xem quyền', 'slug' => 'view-permission',],
+        //     ['name' => 'Tạo mới quyền', 'slug' => 'create-permission',],
+        //     ['name' => 'Sửa quyền','slug' => 'edit-permission', ],
+        //     ['name' => 'Cập nhật quyền', 'slug' => 'update-permission'],
+        //     ['name' => 'Xóa quyền', 'slug' => 'delete-permission',]
+        //  ]);
 
-            ['name' => 'Xem quyền', 'slug' => 'view-permission',],
-            ['name' => 'Tạo mới quyền', 'slug' => 'create-permission',],
-            ['name' => 'Sửa quyền','slug' => 'edit-permission', ],
-            ['name' => 'Cập nhật quyền', 'slug' => 'update-permission'],
-            ['name' => 'Xóa quyền', 'slug' => 'delete-permission',]
-
-         ]);
+        //  DB::table('roles')->insert([
+        //     ['name'=>'Super admin','slug'=>'SUPER ADMIN'],
+        //     ['name'=>'admin','slug'=>'ADMIN'],
+        //     ['name'=>'Nhân viên','slug'=>'EMPLOYEE'],
+        //     ['name'=>'Nguời dùng','slug'=>'USER']
+        //     ,
+        // ]);
+        // for($i=1;$i<36;$i++){
+        //     DB::table('role_permission')->insert([
+        //         ['role_id'=>1,'permission_id'=>$i],
+        //     ]);
+        // }
         // $
         // return [
         //     'name' => $name,
--- Book/database/seeders/PermissionSeed.php
+++ Book/database/seeders/PermissionSeed.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use App\Models\Permission;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class PermissionSeed extends Seeder
 {
@@ -15,6 +16,26 @@ class PermissionSeed extends Seeder
     public function run()
     {
         // factory(App\Models\Permission::class, 10)->create();
-        Permission::factory()->count(10)->create();
+        // Permission::factory()->count(10)->create();
+        DB::table('permissions')->insert([
+            ['name' => 'Xem quyền', 'slug' => 'view-permission',],
+            ['name' => 'Tạo mới quyền', 'slug' => 'create-permission',],
+            ['name' => 'Sửa quyền','slug' => 'edit-permission', ],
+            ['name' => 'Cập nhật quyền', 'slug' => 'update-permission'],
+            ['name' => 'Xóa quyền', 'slug' => 'delete-permission',]
+         ]);
+
+         DB::table('roles')->insert([
+            ['name'=>'Super admin','slug'=>'SUPER ADMIN'],
+            ['name'=>'admin','slug'=>'ADMIN'],
+            ['name'=>'Nhân viên','slug'=>'EMPLOYEE'],
+            ['name'=>'Nguời dùng','slug'=>'USER']
+            ,
+        ]);
+        for($i=1;$i<36;$i++){
+            DB::table('role_permission')->insert([
+                ['role_id'=>1,'permission_id'=>$i],
+            ]);
+        }
     }
 }
